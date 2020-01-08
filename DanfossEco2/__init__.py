4e1eeab6ca0cbdcc992bd7f27a96040d02856d70
GylleTanken <5215873+GylleTanken@users.noreply.github.com>
2020-01-08 01:02:12 +0000

    Added context manager

--- DanfossEco2/__init__.py
+++ DanfossEco2/__init__.py
@@ -7,6 +7,7 @@ from bluepy import btle
 import time
 import xxtea
 import sys
+import threading
 
 class DanfossEco2(btle.DefaultDelegate):
 
@@ -16,133 +17,121 @@ class DanfossEco2(btle.DefaultDelegate):
     battery_uuid = "2A19"
 
     max_send_time = 30
-    max_connect_time = 40
-    connected = False
 
-    def __init__(self, mac="00:04:2F:C1:02:A2", encryption_key=b'\xA5\x7F\xF7\x6F\xE8\x6D\xA0\x65\x77\x91\x15\xC2\xA6\x7E\x4A\x92', pin=0000):
+    def __init__(self, mac="00:04:2F:C1:02:A2", encryption_key=b'\xA5\x7F\xF7\x6F\xE8\x6D\xA0\x65\x77\x91\x15\xC2\xA6\x7E\x4A\x92', pin=0000, max_connect_time=60):
         self.mac = mac
         self.pin = pin
         self.key = encryption_key
+        self.max_connect_time = max_connect_time
 
         self.pin_handle = None
         self.temperature_handle = None
         self.device_name_handle = None
         self.battery_handle = None
+
+        self.device = None
 
         self.battery = 0
         self.name = ""
         self.current_temperature = 0
-        self.set_temperature = 0
+        self.target_temperature = 0
+
+        self.mutex = threading.Lock()
 
         btle.DefaultDelegate.__init__(self)
-        self.connected = False
-
-    def __del__(self):
-        self.disconnect()
 
 
-    def connect(self):
-        self.disconnect()
 
-        initial = time.time()
+    def __enter__(self):
+        self.mutex.acquire()
+        self.device = btle.Peripheral()
+        self.device.withDelegate(self)
+
+        start_time = time.time()
         connection_try_count = 0
+
         while True:
-            if time.time() - initial >= self.max_connect_time:
-                return False
             connection_try_count += 1
             if connection_try_count > 1:
-                print("Trying to connect again. Try number: " + str(connection_try_count))
+                print("Retrying to connect to device with mac: " + self.mac + ", try number: " + str(connection_try_count))
             try:
-                self.device = btle.Peripheral(self.mac, addrType=btle.ADDR_TYPE_PUBLIC)
-            except:
-                print("Could not connect to device with mac: " + self.mac)
-                print("Error: ", sys.exc_info()[0])
-                continue
+                self.device.connect(self.mac, addrType=btle.ADDR_TYPE_PUBLIC)
+                break
+            except btle.BTLEException as ex:
+                print("Could not connect to device with mac: " + self.mac + ", error: " + str(ex))
+                if time.time() - start_time >= self.max_connect_time:
+                    print("Connection timeout")
+                    raise
 
-            self.device.withDelegate(self)
-            handles = self.device.getCharacteristics()
-            for handle in handles:
-                if handle.uuid == self.pin_uuid:
-                    self.pin_handle = handle
-                elif handle.uuid == self.temperature_uuid:
-                    self.temperature_handle = handle
-                elif handle.uuid == self.device_name_uuid:
-                    self.device_name_handle = handle
-                elif handle.uuid == self.battery_uuid:
-                    self.battery_handle = handle
-        
-            if self.pin_handle == None or self.temperature_handle == None or self.device_name_handle == None or self.battery_handle == None:
-                print("Could not find all handles")
-                return False
-            else:
-                self.connected = True
-                return True
+        handles = self.device.getCharacteristics()
+        for handle in handles:
+            if handle.uuid == self.pin_uuid:
+                self.pin_handle = handle
+            elif handle.uuid == self.temperature_uuid:
+                self.temperature_handle = handle
+            elif handle.uuid == self.device_name_uuid:
+                self.device_name_handle = handle
+            elif handle.uuid == self.battery_uuid:
+                self.battery_handle = handle
 
+        if self.pin_handle == None or self.temperature_handle == None or self.device_name_handle == None or self.battery_handle == None:
+            self.device = None
+            raise Exception("Unable to find all handles")
 
+        self.login()
 
-    def disconnect(self):
-        self.pin_handle = None
-        self.temperature_handle = None
-        self.device_name_handle = None
-        self.battery_handle = None
-        if hasattr(self, 'device'):
-            self.device.disconnect()
-        self.connected = False
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            if self.device:
+                self.device.disconnect()
+        except Exception as ex:
+            print("Error disconnecting: " + str(ex))
+        finally:
+            self.device = None
+            self.pin_handle = None
+            self.temperature_handle = None
+            self.device_name_handle = None
+            self.battery_handle = None
+            self.mutex.release()
+
 
     def login(self):
-        if self.connected:
-            return self.send_BLE_packet(self.pin_handle, b'\x00\x00\x00\x00')
-        else:
-            return False
+        return self.send_BLE_packet(self.pin_handle, b'\x00\x00\x00\x00')
 
     def getTemperature(self):
-        if self.connected:
-            data = self.read_encrypted_handle(self.temperature_handle)
-            if data[1] != 0:
-                self.current_temperature = data[1]/2
-            self.set_temperature = data[0]/2
-            return (self.set_temperature, self.current_temperature)
-        else:
-            return False
+        data = self.read_encrypted_handle(self.temperature_handle)
+        if data[1] != 0:
+            self.current_temperature = data[1]/2
+        self.target_temperature = data[0]/2
+        return (self.target_temperature, self.current_temperature)
 
     def getBattery(self):
-        if self.connected:
-            data = self.battery_handle.read()
-            self.battery = data[0]
-            return self.battery
-        else:
-            return False
+        data = self.battery_handle.read()
+        self.battery = data[0]
+        return self.battery
 
     def getDeviceName(self):
-        if self.connected:
-            data = self.read_encrypted_handle(self.device_name_handle)
-            for i, char in enumerate(data):
-                if char == 0:
-                    data = data[:i]
-                    break
-            self.device_name = data.decode("utf-8")
-            return self.device_name
-        else:
-            return False
+        data = self.read_encrypted_handle(self.device_name_handle)
+        for i, char in enumerate(data):
+            if char == 0:
+                data = data[:i]
+                break
+        self.device_name = data.decode("utf-8")
+        return self.device_name
 
     def setTemperature(self, temp):
-        if self.connected:
-            data = bytearray([round(temp*2)]) + bytearray(7)
-            return self.send_encrypted_packet(self.temperature_handle, data)
-        else:
-            return False
+        data = bytearray([round(temp*2)]) + bytearray(7)
+        return self.send_encrypted_packet(self.temperature_handle, data)
 
     def update(self):
-        self.connect()
-        if self.connected:
-            self.login()
-            self.getDeviceName()
-            self.getBattery()
-            self.getTemperature()
-            self.disconnect()
-            return True
-        else:
+        if self.getBattery() == False:
             return False
+        elif self.getTemperature() == False:
+            return False
+        else:
+            return True
 
     def send_encrypted_packet(self, handle, unencrypted_data):
         return self.send_BLE_packet(handle, self.encrypt(unencrypted_data))
@@ -151,14 +140,7 @@ class DanfossEco2(btle.DefaultDelegate):
         return self.decrypt(handle.read())
 
     def send_BLE_packet(self, handle, data):
-        initial = time.time()
-        while True:
-            if time.time() - initial >= self.max_send_time:
-                return False
-            try:
-                return handle.write(bytes(data), withResponse=True)
-            except:
-                self.connect()
+        return handle.write(bytes(data), withResponse=True)
 
     def encrypt(self, data):
         return self.reverse_byte_order(xxtea.encrypt(bytes(self.reverse_byte_order(data)), self.key, padding=False))
